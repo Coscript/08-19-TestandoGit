319af332811a204f255cbbab9d22eefa94fb2213
Eduardo Viana <eduardo.carrinhos@hotmail.com>
2021-09-09 11:10:35 -0400

    Routes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View} from 'react-native';
-import Home from './src/Pages/Home';
+import { Text, View, StyleSheet} from 'react-native';
+import Routes from './src/routes';
 
 
 export default function App() {
   return (
-    <View> 
-      <Home/>
+    <View style={styles.container}> 
+      
+      <Routes/>
+
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
